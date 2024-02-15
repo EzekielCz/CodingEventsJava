d73293e27b91fd85004f981c54a6f8712c0df90b
EzekielCz <ezechielcazacu@gmail.com>
2024-02-14 21:52:49 -0600

    authentication

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,65 +1,80 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Chris Bay
  */
 @Entity
-public class Event extends AbstractEntity  {
+public class Event extends AbstractEntity {
 
-    @NotBlank(message = "Name is required")
-    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//    private static int nextId = 1;
+
+    @NotBlank(message = "Cannot be emtpy.")
+    @Size(min = 3, max = 25, message = "Name must be between 3 and 25 characters long.")
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
+    @Size(max = 250, message = "Description is too long.")
     private String description;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email. Try again.")
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email, please try again")
     private String contactEmail;
 
     @ManyToOne
-    @NotNull(message = "Category is required")
+    @NotNull(message = "Category is required.")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    private final List<Tag> tags =new ArrayList<>();
+    public Event(String name, EventCategory eventCategory) {
+//        this();
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
+//        this.description = description;
+//        this.contactEmail = contactEmail;
         this.eventCategory = eventCategory;
     }
-
-    public Event() {}
-
+    public Event(){
+//        this.id = nextId;
+//        nextId++;
+    };
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
+//    public String getDescription() {
+//        return description;
+//    }
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getContactEmail() {
+//        return contactEmail;
+//    }
+//
+//    public void setContactEmail(String contactEmail) {
+//        this.contactEmail = contactEmail;
+//    }
 
     public EventCategory getEventCategory() {
         return eventCategory;
@@ -69,9 +84,25 @@ public class Event extends AbstractEntity  {
         this.eventCategory = eventCategory;
     }
 
-    @Override
-    public String toString() {
-        return name;
+    public EventDetails getEventDetails() {
+        return eventDetails;
     }
 
-}
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+    public void addTags (Tag tag){
+        this.tags.add(tag);
+    }
+
+    //    public int getId() {
+//        return id;
+//    }
+//
+    @Override
+    public String toString() {
+        return name;}}
